3aa602dbbffdbdecdfa4fefa3d25b1302748c6e1
José Ordoñez <restonjd@gmail.com>
2013-09-05 17:36:17 -0430

    sitemap and webmaster tools

--- showtimeDuranjo/urls.py
+++ showtimeDuranjo/urls.py
@@ -1,11 +1,22 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-from sitemaps import StaticViewSitemap
+from sitemaps import StaticViewSitemap, VideoSitemap, AlbumSitemap
+from django.http import HttpResponse
+from zinnia.sitemaps import TagSitemap
+from zinnia.sitemaps import EntrySitemap
+from zinnia.sitemaps import CategorySitemap
+from zinnia.sitemaps import AuthorSitemap
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 sitemaps = {
 	'pages': StaticViewSitemap,
+	'tags': TagSitemap,
+	'blog': EntrySitemap,
+	'authors': AuthorSitemap,
+	'categories': CategorySitemap,
+	'videos': VideoSitemap,
+	'albumes': AlbumSitemap,
 }
 
 urlpatterns = patterns(
@@ -19,7 +30,10 @@ urlpatterns = patterns(
 	url(r'^comments/', include('django.contrib.comments.urls')),
 	# Uncomment the admin/doc line below to enable admin documentation:
 	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+	url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+	url(r'^sitemap-(?P<section>.+)\.xml$', 'sitemap', {'sitemaps': sitemaps}),
+	#Google Web master Tool
+	(r'^googlec34fe789f50fc843\.html$', lambda r: HttpResponse("google-site-verification: googlec34fe789f50fc843.html", mimetype="text/plain")),
 	# Uncomment the next line to enable the admin:
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='admin_password_reset'),
