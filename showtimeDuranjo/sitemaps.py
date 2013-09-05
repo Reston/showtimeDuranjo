9831360003f92a65b6defc958c3095864c509cf6
José Ordoñez <restonjd@gmail.com>
2013-09-05 17:36:29 -0430

    admin site statics files

--- showtimeDuranjo/sitemaps.py
+++ showtimeDuranjo/sitemaps.py
@@ -1,7 +1,7 @@
 #-*- encoding: utf-8 -*-
 from django.contrib import sitemaps
 from django.core.urlresolvers import reverse
-
+from showtimeDuranjo.apps.galeria.models import Video, Album
 
 class StaticViewSitemap(sitemaps.Sitemap):
 	priority = 0.5
@@ -9,9 +9,33 @@ class StaticViewSitemap(sitemaps.Sitemap):
 
 	def items(self):
 		return [
-				#'homepageindex',
+				'homepageindex',
+				'homepageradio',
+				'homepagecontacto',
+				'galeriagaleria',
 				# colocar los nombre de las url en este lugar. ejemplo: 'homepageworks'
 				]
 
 	def location(self, item):
 		return reverse(item)
+
+class VideoSitemap(sitemaps.Sitemap):
+	changefreq = 'monthly'
+	priority = 0.6
+
+	def items(self):
+		return Video.objects.all()
+
+	def lastmod(self, obj):
+		return obj.modificado_en
+
+
+class AlbumSitemap(sitemaps.Sitemap):
+	changefreq = 'monthly'
+	priority = 0.6
+
+	def items(self):
+		return Album.objects.all()
+
+	def lastmod(self, obj):
+		return obj.modificado_en
